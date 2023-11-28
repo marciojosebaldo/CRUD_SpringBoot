9a83ff046d393fe29a9fcc17786e5352f051c996
marciojosebaldo <marciojosebaldo@gmail.com>
2023-11-27 22:04:36 -0300

    produto

--- src/main/java/CRUD/CRUD/com/Spring/Boot/Service/S_Produto.java
+++ src/main/java/CRUD/CRUD/com/Spring/Boot/Service/S_Produto.java
@@ -3,6 +3,7 @@ package CRUD.CRUD.com.Spring.Boot.Service;
 import CRUD.CRUD.com.Spring.Boot.Model.M_Produto;
 import CRUD.CRUD.com.Spring.Boot.Repository.R_Produto;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
 public class S_Produto {
@@ -13,7 +14,11 @@ public class S_Produto {
         this.r_produto = r_produto;
     }
 
-    public static M_Produto cadastroProduto() {
+    public static M_Produto cadastroProduto(@RequestParam("nome") String nome,
+                                            @RequestParam("quantidade") int quantidade,
+                                            @RequestParam("valor") float valor) {
+        M_Produto m_produto;
+
         return null;
     }
 }
--- src/main/java/CRUD/CRUD/com/Spring/Boot/Service/S_Usuario.java
+++ src/main/java/CRUD/CRUD/com/Spring/Boot/Service/S_Usuario.java
@@ -20,5 +20,4 @@ public class S_Usuario {
 
         return r_usuario.buscarUsuarioPorEmailSenha(email, senha);
     }
-
 }
