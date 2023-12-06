6306367b0d0ede2a94f17dbc55170497947fb3b3
marciojosebaldo <marciojosebaldo@gmail.com>
2023-12-06 19:23:08 -0300

    visualizarProdutos

--- target/classes/static/JS/produto.js
+++ target/classes/static/JS/produto.js
@@ -1,6 +1,6 @@
 $("#cadastrar").click(cadastroProduto);
 
-function cadastroProduto(){
+function cadastroProduto() {
     let nome = $("#nome").val();
     let quantidade = $("#quantidade").val();
     let valor = $("#valor").val();
@@ -13,25 +13,34 @@ function cadastroProduto(){
             quantidade: quantidade,
             valor: valor
         },
-        success: function(data){
+        success: function (data) {
             alert(data);
 
-            if(data) {
-                window.location.href="/CRUD/home";
+            if (data) {
+                window.location.href = "/CRUD/home";
             } else {
                 alert("Não foi possível cadastrar o produto")
             }
         },
-        error: function(){
+        error: function () {
             alert("Falha ao cadastrar o produto");
         }
     });
 }
 
+$("#visualizar").click(visualizarProduto);
+
+function visualizarProduto() {
+    $.ajax({
+        type: "GET",
+
+    })
+}
+
 
 $("#excluir").click(excluirProduto);
 
-function excluirProduto(){
+function excluirProduto() {
     let id = $("#id").val();
 
     $.ajax({
@@ -40,42 +49,22 @@ function excluirProduto(){
         data: {
             id: id
         },
-        success: function(data) {
+        success: function (data) {
             alert(data);
 
-            if(data) {
-                window.location.href="/CRUD/home";
+            if (data) {
+                window.location.href = "/CRUD/home";
             } else {
                 alert("Não foi possível excluir o produto")
             }
         },
-        error: function(){
+        error: function () {
             alert("Falha ao excluir o produto");
         }
     });
 }
 
-
-$("#visualizar").click(function () {
-    $("#tabelaProdutos").load("/CRUD/tabelaProdutos", function() {
-        botoesEditarExcluir();
-    });
-});
-
-function botoesEditarExcluir() {
-    $(".btn btn-editar").off("click");
-    $(".btn btn-excluir").off("click");
-
-    $(".btn-editar").click(function () {
-
-    });
-
-    $(".btn-excluir").click(function () {
-
-    });
-}
-
 $("#atualizar").click(atualizarProduto);
 
-function atualizarProduto(){
+function atualizarProduto() {
 }
